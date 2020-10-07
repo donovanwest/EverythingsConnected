a0f90d212a2cdfd7b9797e0fb58a2a6883e08d3e
Donovan West <donovanrwest@gmail.com>
2020-10-06 21:38:55 -0500

    Still trying to add nodes

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const height = svg.attr("height");
 
 let time1 = Date.now();
 let time2 = 0;
-let nodeNum = 8;
+let nodeNum=0;
+let temp = 0;
 
   //intialize data
-  const graph = {
+  let graph = {
     nodes: [
       { id: 0 },
       { id: 1 },
@@ -27,22 +28,112 @@ let nodeNum = 8;
       { source: 7, target: 6 }
     ]
   };
+console.log(graph);
+const simulation = d3
+  .forceSimulation(graph.nodes)
+  .force("link", d3.forceLink(graph.links))
+  .force("charge", d3.forceManyBody().strength(-100))
+  .force("center", d3.forceCenter(width / 2, height / 2).strength(1))
+  .on("tick", ticked);
+
+
+let link = svg.append("g")
+  .attr("stroke", "#999")
+  .attr("class", "link")
+  .selectAll("line")
+  .data(graph.links)
+  .join("line")
+  .attr("stroke-width", 3);
+
+let node = svg
+  .append("g")
+  .attr("class", "node")
+  .selectAll(".node")
+  .data(graph.nodes)
+  .enter()
+  .append("circle")
+  .attr("r", 10)
+  .attr("class", "node")
+  .attr("fill", "red");
+
+function ticked() {
+  time2 = Date.now();
+  if(time2 - time1 > 2000){
+    time1 = Date.now();
+    addNodes();
+  }
+  link
+    .attr("x1", function(d) {
+      return d.source.x;
+    })
+    .attr("y1", function(d) {
+      return d.source.y;
+    })
+    .attr("x2", function(d) {
+      return d.target.x;
+    })
+    .attr("y2", function(d) {
+      return d.target.y;
+    });
+
+  node
+    .attr("cx", function(d) {
+      return d.x;
+    })
+    .attr("cy", function(d) {
+      return d.y;
+    });
+}
+
+const drag = d3
+  .drag()
+  .on("start", dragstarted)
+  .on("drag", dragged)
+  .on("end", dragended);
+  node.call(drag);
+function dragstarted(event) {
+  //your alpha hit 0 it stops! make it run again
+  if (!event.active) simulation.alphaTarget(0.3).restart();
+  event.subject.fx = event.subject.x;
+  event.subject.fy = event.subject.y;
+}
+function dragged(event) {
+  event.subject.fx = event.x;
+  event.subject.fy = event.y;
+}
+
+function dragended(event) {
+  // alpha min is 0, head there
+  if (!event.active) simulation.alphaTarget(0);
+  event.subject.fx = null;
+  event.subject.fy = null;
+  
+}
+
+function addNodes(){
   console.log(graph);
-  const simulation = d3
-    .forceSimulation(graph.nodes)
-    .force("link", d3.forceLink(graph.links))
-    .force("charge", d3.forceManyBody().strength(-100))
-    .force("center", d3.forceCenter(width / 2, height / 2).strength(1))
-    .on("tick", ticked);
+  console.log("pushing node " + nodeNum);
+  // graph.nodes.push({id: nodeNum});
+  //graph.links.push({source: nodeNum, target: (Math.floor((Math.random() * nodeNum)))})
+  nodeNum = nodeNum+1;
 
-  let link = svg.append("g")
-    .attr("stroke", "#999")
-    .selectAll("line")
-    .data(graph.links)
-    .join("line")
-    .attr("stroke-width", 3);
+  node = svg
+  //.append("g")
+  //.attr("class", "node")
+  .selectAll("node")
+  .data(graph.nodes)
+  .enter()
+  .append("circle")
+  .attr("class", "node")
+  .attr("r", 10)
 
-  let node = svg
+  node.exit().remove();
+
+  //node.exit().remove();
+  //simulation.restart();
+
+/*    
+  node = svg
     .append("g")
     .attr("class", "nodes")
     .selectAll("circle")
@@ -50,83 +141,33 @@ let nodeNum = 8;
     .enter()
     .append("circle")
     .attr("r", 10)
-    .attr("fill", "red");
-  
-  function ticked() {
-    time2 = Date.now();
-    if(time2 - time1 > 2000){
-      time1 = Date.now();
-      console.log("pushing node " + nodeNum);
-      graph.nodes.push({id: nodeNum});
-      graph.links.push({source: nodeNum, target: (Math.floor((Math.random() * nodeNum)))})
-      nodeNum++;
-      console.log(graph);
-      restart();
-    }
-    link
-      .attr("x1", function(d) {
-        return d.source.x;
-      })
-      .attr("y1", function(d) {
-        return d.source.y;
-      })
-      .attr("x2", function(d) {
-        return d.target.x;
-      })
-      .attr("y2", function(d) {
-        return d.target.y;
-      });
+    .attr("fill", "green");
+  simulation.alpha(0.3).restart();
 
-    node
-      .attr("cx", function(d) {
-        return d.x;
-      })
-      .attr("cy", function(d) {
-        return d.y;
-      });
-  }
-  
-  const drag = d3
-    .drag()
-    .on("start", dragstarted)
-    .on("drag", dragged)
-    .on("end", dragended);
-    node.call(drag);
-  function dragstarted(event) {
-    //your alpha hit 0 it stops! make it run again
-    if (!event.active) simulation.alphaTarget(0.3).restart();
-    event.subject.fx = event.subject.x;
-    event.subject.fy = event.subject.y;
-  }
-  function dragged(event) {
-    event.subject.fx = event.x;
-    event.subject.fy = event.y;
-  }
+  link = svg.append("g")
+    .attr("stroke", "#999")
+    .selectAll("line")
+    .data(graph.links)
+    .join("line")
+    .attr("stroke-width", 3); */
+  //simulation.alpha(0.3).restart();
+}
+/*
+function restart() {
+  node = node.data(graph.nodes);
+  node.enter().insert("circle")
+      .attr("class", "nodes")
+      .attr("r", 10)
+      .attr("fill", "green");
+  node.exit().remove();
 
-  function dragended(event) {
-    // alpha min is 0, head there
-    if (!event.active) simulation.alphaTarget(0);
-    event.subject.fx = null;
-    event.subject.fy = null;
-    
-  }
+  link = link.data(graph.links);
+  link.enter().insert("line")
+      .attr("class", "links")
+      .attr("stroke", "#999")
+      .attr("stroke-width", 3);
+  link.exit().remove();
 
-  function restart() {
-    node = node.data(graph.nodes);
-    node.enter().insert("circle")
-        .attr("class", "nodes")
-        .attr("r", 10)
-        .attr("fill", "green");
-    node.exit().remove();
-
-    link = link.data(graph.links);
-    link.enter().insert("line")
-        .attr("class", "links")
-        .attr("stroke", "#999")
-        .attr("stroke-width", 3);
-    link.exit().remove();
-
-    simulation.restart();
-  }
-
- 
+  simulation.restart();
+}
+*/
